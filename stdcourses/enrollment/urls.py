786454de13038e8f79b2263e837508684e1dfa94
Nikhitha A <nikhithaananth282@gmail.com>
2024-07-25 00:34:33 +0530

    Add files via upload

---
+++ stdcourses/enrollment/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('register/', views.register_student, name='register_student'),
+    path('generate_csv/', views.generate_csv, name='generate_csv'),
+    path('generate_pdf/', views.generate_pdf, name='generate_pdf'),
+]
+
